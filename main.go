675f4a4de1bf802e565cbb292fbbf874cc93a7d0
Garrett Halstein <garrett.halstein@emory.edu>
2019-11-11 21:16:06 -0500

    Hello World in Golang

---
+++ main.go
@@ -0,0 +1,10 @@
+// main executable package
+package main
+
+// importing format package
+import "fmt"
+
+// main function printing hello world to terminal
+func main() {
+	fmt.Println("Hello World!")
+}
